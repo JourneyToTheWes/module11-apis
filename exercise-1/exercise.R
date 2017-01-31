29032cda2035494bcdd70dd1f2efcc8708f18805
Wesley Wei <wesley631w@gmail.com>
2017-01-31 15:19:28 -0800

    Tried GET() for API data

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -1,11 +1,13 @@
 ### Exercise 1 ###
 
 # Load the httr and jsonlite libraries for accessing data
-
-
+library(httr)
+library(jsonlite)
 ## For these questions, look at the API documentation to identify the appropriate endpoint and information.
 ## Then send GET() request to fetch the data, then extract the answer to the question
-
+response <- GET("http://data.unhcr.org/wiki/index.php/API_Documentation.html")
+body <- content(response, "text")
+parsed.data <- fromJSON(body)
 # For what years does the API have statistical data?
 
 
